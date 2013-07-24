0541f28cc16b414aed7ec2bef577d0d34beef6f1
linuxium <linuxium@live.com>
2013-07-25 00:26:45 +1000

    first commit

---
+++ kernel_ruikemei/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 275 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#275 SMP PREEMPT Fri Jul 19 14:56:59 CST 2013"
+#define LINUX_COMPILE_BY "hotack"
+#define LINUX_COMPILE_HOST "adminroot-X9DRL-3F-iF"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
